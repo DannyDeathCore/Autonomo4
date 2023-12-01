f7fcef6c02801ba7e7edf7a4ae549e602f88dd27
amandya3@gmil.com <amandya3@gmail.com>
2023-12-01 12:01:43 -0500

    estoy comiteando los archivos

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    // Manejar clic en el enlace de "Contacto"
+    $("#contacto-link").click(function () {
+        // Mostrar la sección de contacto
+        $("#contacto").show();
+    });
+});
