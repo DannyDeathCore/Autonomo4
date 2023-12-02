1972a2ae4eff4414354a14efaaa04216912a8ce7
DannyDeathCore <djcortez3@tes.edu.ec>
2023-12-02 18:49:16 -0500

    recursos

--- complemento/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    // Manejar clic en el enlace de "Contacto"
-    $("#contacto-link").click(function () {
-        // Mostrar la sección de contacto
-        $("#contacto").show();
-    });
-});
