6aa07302a3139773a4c8b722977852f2cd2954c2
DannyDeathCore <djcortez3@tes.edu.ec>
2023-12-02 21:10:51 -0500

    Correcciones

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,27 @@ $(document).ready(function () {
     // Manejar clic en el enlace de "Contacto"
     $("#contacto-link").click(function () {
         // Mostrar la sección de contacto
-        $("#contacto").show();
+        $("#contactos.html").show();
+    });
+});
+$(document).ready(function () {
+    // Manejar clic en el enlace de "Contacto"
+    $("#nosotros-link").click(function () {
+        // Mostrar la sección de contacto
+        $("#sobrenosotros.html").show();
+    });
+});
+$(document).ready(function () {
+    // Manejar clic en el enlace de "Contacto"
+    $("#productos-link").click(function () {
+        // Mostrar la sección de contacto
+        $("#productos.html").show();
+    });
+});
+$(document).ready(function () {
+    // Manejar clic en el enlace de "Contacto"
+    $("#inicio-link").click(function () {
+        // Mostrar la sección de contacto
+        $("#index.html").show();
     });
 });
